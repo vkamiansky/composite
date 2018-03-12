70ed84e808ed9d6b60c9185bddfd4d375b7608f4
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2018-03-13 01:35:22 +0300

    Invalid xml in documentation fixed

--- Composite/Composite/C.fs
+++ Composite/Composite/C.fs
@@ -4,7 +4,7 @@ open System.Collections.Generic
 
 module C =
 
-    ///<summary>Instantiates a new <c>Composite</c> variant of <c>Composite<T></c>.
+    ///<summary>Instantiates a new <c>Composite</c> variant of <c>Composite&lt;T&gt;</c>.</summary>
     ///<param name="innerForest">The inner forest to wrap in a <c>Composite</c>.</param>
     ///<typeparam name="T">The type of payload elements in the <c>Composite</c>.</typeparam>
     ///<exception cref="System.ArgumentNullException">Thrown when <c>innerForest</c> is null.</exception>
@@ -13,7 +13,7 @@ module C =
         then nullArg "innerForest"
         else Composite innerForest
 
-    ///<summary>Instantiates a new <c>Value</c> variant of <c>Composite<T></c>.
+    ///<summary>Instantiates a new <c>Value</c> variant of <c>Composite&lt;T&gt;</c>.</summary>
     ///<param name="value">The payload object.</param>
     ///<typeparam name="T">The type of the payload object.</typeparam>
     let Value (value: 'T) =
